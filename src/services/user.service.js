eddc6ed822a459044601b03876380e1ba46bf526
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2022-06-07 16:17:22 +0800

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,7 +3,7 @@ import authHeader from './auth-header';
 
 class UserService {
   getProfile() {
-    return api.post('/api/user/profile', {}, { headers: authHeader() })
+    return api.get('/api/user/profile', { headers: authHeader() })
       .then(response => {
         return response.data.profile;
       });
